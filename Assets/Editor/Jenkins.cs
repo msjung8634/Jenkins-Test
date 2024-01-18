6d6d7d0df926ed292f3abbcafc9b989a22202270
msjung8634 <msjung8634@gmail.com>
2024-01-18 15:49:17 +0900

    initial copy

---
+++ Assets/Editor/Jenkins.cs
@@ -0,0 +1,71 @@
+﻿// Assets/Editor/Jenkins.cs
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Jenkins
+{
+    [MenuItem("Build/Standalone Windows")]
+    public static void PerformBuild_Windows()
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        // 씬 추가
+        List<string> scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            scenes.Add(scene.path);
+        }
+        options.scenes = scenes.ToArray();
+        // 타겟 경로(빌드 결과물이 여기 생성됨)
+        options.locationPathName = "Build/BuildTesting.exe";
+        // 빌드 타겟
+        options.target = BuildTarget.StandaloneWindows;
+
+        // 빌드
+        BuildPipeline.BuildPlayer(options);
+    }
+    [MenuItem("Build/Server_Build")]
+    public static void PerformBuild_Server()
+    {
+
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        // 씬 추가
+        List<string> scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            scenes.Add(scene.path);
+        }
+        options.scenes = scenes.ToArray();
+        // 타겟 경로(빌드 결과물이 여기 생성됨)
+        options.locationPathName = "Build/Server/Server.exe";
+        // 빌드 타겟
+        options.target = BuildTarget.Android;
+        options.options = BuildOptions.EnableHeadlessMode;
+
+        // 빌드
+        BuildPipeline.BuildPlayer(options);
+    }
+    [MenuItem("Build/Android")]
+    public static void PerformBuild_Android()
+    {
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        // 씬 추가
+        List<string> scenes = new List<string>();
+        foreach (var scene in EditorBuildSettings.scenes)
+        {
+            if (!scene.enabled) continue;
+            scenes.Add(scene.path);
+        }
+        options.scenes = scenes.ToArray();
+        // 타겟 경로(빌드 결과물이 여기 생성됨)
+        options.locationPathName = "Build/BuildTesting.apk";
+        // 빌드 타겟
+        options.target = BuildTarget.Android;
+
+        // 빌드
+        BuildPipeline.BuildPlayer(options);
+    }
+}
